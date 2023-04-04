0c6020d8a69da2bd0a502dfaf39d96c33e3e4b6c
ray <rizkyray01@gmail.com>
2023-04-04 22:02:28 +0800

    redo Matkul card

--- resources/views/components/matkul.blade.php
+++ resources/views/components/matkul.blade.php
@@ -1,20 +1,8 @@
 <div class="col-lg-4 col-md-6 mb-5">
-    <div class="card shadow-sm">
+    <div class="card shadow-sm ">
         <img src="{{ asset('img/blender.jpg') }}" class="card-img-top" alt="computer-aided">
-        <div id="text" class="card-body">
+        <div id="text" class="card-body h-100">
             {{-- <span class="badge bg-secondary badge-lg ">SISTEM INFORMASI</span> --}}
-            <span class="badge bg-secondary">SISTEM INFORMASI</span>
-            <h5 class="card-title mt-1"><a href="/matkul/materi" class="judul-matkul">Computer Aided Design</a></h5>
-            <p class="card-text">Some quick example text to build on the card title</p>
+            {{ $slot }}
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item"><i class="fa-regular fa-folder"></i></i> 10 Materi</li>
-            <li class="list-group-item">
-                <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0"
-                    aria-valuemax="100">
-                    <div class="progress-bar bg-danger" style="width: 75%"></div>
-                </div>
-            </li>
-        </ul>
     </div>
-</div>
