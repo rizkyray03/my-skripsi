f7c4402d27485268050ecec2a67c4ee672264f6c
ray <rizkyray01@gmail.com>
2023-05-06 15:39:06 +0800

    fix: calendar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,20 +1,20 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light ml-5">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
 
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
+            <a href="/" class="nav-link">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Dashboard</a>
+            <a href="/dashboard" class="nav-link">Dashboard</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Mata Kuliah</a>
+            <a href="/matkul" class="nav-link">Mata Kuliah</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Manual</a>
+            <a href="/manual" class="nav-link">Manual</a>
         </li>
     </ul>
 
