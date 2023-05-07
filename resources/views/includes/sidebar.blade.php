607b23b5fd61c2ba8a17a1da7528562cb6cb3a27
ray <rizkyray01@gmail.com>
2023-05-07 21:39:08 +0800

    default sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -6,18 +6,9 @@
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
-    <div class="sidebar">
+    <div class="sidebar mx-2">
 
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
-        <div class="form-inline">
+        <div class="form-inline mt-2">
             <div class="input-group" data-widget="sidebar-search">
                 <input class="form-control form-control-sidebar" type="search" placeholder="Search"
                     aria-label="Search">
@@ -46,8 +37,7 @@
                     <a href="pages/widgets.html" class="nav-link">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
-                            Widgets
-                            <span class="right badge badge-danger">New</span>
+                            Mata Kuliah
                         </p>
                     </a>
                 </li>
