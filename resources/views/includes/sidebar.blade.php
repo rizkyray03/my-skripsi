c311fabebd1ed0e87d418001ad5329bed3b4e712
ray <rizkyray01@gmail.com>
2023-05-07 21:50:50 +0800

    remove old sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -7,57 +7,16 @@
     </a>
 
     <div class="sidebar">
-
-        {{-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
-            </div>
-        </div>
-
-        <div class="form-inline">
-            <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                    aria-label="Search">
-                <div class="input-group-append">
-                    <button class="btn btn-sidebar">
-                        <i class="fas fa-search fa-fw"></i>
-                    </button>
-                </div>
-            </div>
-        </div> --}}
-
+        {{-- Moodle sidebar --}}
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
-
-                {{-- <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
-                        <i class="nav-icon fas fa-home-alt"></i>
-                        <p>
-                            Home
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="pages/widgets.html" class="nav-link">
-                        <i class="nav-icon fas fa-calendar"></i>
-                        <p>
-                            Calendar
-                            <span class="right badge badge-danger">New</span>
-                        </p>
-                    </a>
-                </li> --}}
-
-                <li>
+                <li class="nav-item menu-open">
                     <a class="list-group-item list-group-item-action  " href="/" data-key="home">
                         <div class="ml-0">
                             <div class="media">
                                 <span class="media-left">
-                                    <i class="nav-icon fas fa-home fa-fw " aria-hidden="true"></i>
+                                    <i class="nav-icon fas fa-home fa-fw" aria-hidden="true"></i>
                                 </span>
                                 <span class="media-body ">Home</span>
                             </div>
